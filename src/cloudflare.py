e15783325287b45a3f7f8a9c545d3645049f7dde
LexterS999 <kaliostro18@gmail.com>
2024-08-17 23:23:49 +0500

    Update cloudflare.py

--- src/cloudflare.py
+++ src/cloudflare.py
@@ -1,15 +1,12 @@
 import json
-import time
 from src.requests import (
     cloudflare_gateway_request, retry, rate_limited_request, retry_config
 )
-from src import info
+
 
 @retry(**retry_config)
 @rate_limited_request
 def create_list(name, domains):
-    """Создает новый список доменов в Cloudflare Gateway."""
-    start_time = time.time()
     endpoint = "/lists"
     data = {
         "name": name,
@@ -18,31 +15,21 @@ def create_list(name, domains):
         "items": [{"value": domain} for domain in domains]
     }
     status, response = cloudflare_gateway_request("POST", endpoint, body=json.dumps(data))
-    end_time = time.time()
-    info(f"create_list operation took {end_time - start_time:.2f} seconds")
     return response["result"]
-
 
 @retry(**retry_config)
 @rate_limited_request
 def update_list(list_id, remove_items, append_items):
-    """Обновляет существующий список доменов в Cloudflare Gateway."""
-    start_time = time.time()
-    endpoint = f"/lists/{list_id}"
+    endpoint = f"/lists/{list_id}"    
     data = {
-        "remove": list(remove_items),  # Преобразование в список
+        "remove": [domain for domain in remove_items],
         "append": [{"value": domain} for domain in append_items]
-    }
+    }    
     status, response = cloudflare_gateway_request("PATCH", endpoint, body=json.dumps(data))
-    end_time = time.time()
-    info(f"update_list operation took {end_time - start_time:.2f} seconds")
     return response["result"]
-
 
 @retry(**retry_config)
 def create_rule(rule_name, list_ids):
-    """Создает новое правило в Cloudflare Gateway."""
-    start_time = time.time()
     endpoint = "/rules"
     data = {
         "name": rule_name,
@@ -52,15 +39,10 @@ def create_rule(rule_name, list_ids):
         "enabled": True,
     }
     status, response = cloudflare_gateway_request("POST", endpoint, body=json.dumps(data))
-    end_time = time.time()
-    info(f"create_rule operation took {end_time - start_time:.2f} seconds")
     return response["result"]
-
 
 @retry(**retry_config)
 def update_rule(rule_name, rule_id, list_ids):
-    """Обновляет существующее правило в Cloudflare Gateway."""
-    start_time = time.time()
     endpoint = f"/rules/{rule_id}"
     data = {
         "name": rule_name,
@@ -70,51 +52,36 @@ def update_rule(rule_name, rule_id, list_ids):
         "enabled": True,
     }
     status, response = cloudflare_gateway_request("PUT", endpoint, body=json.dumps(data))
-    end_time = time.time()
-    info(f"update_rule operation took {end_time - start_time:.2f} seconds")
     return response["result"]
-
 
 @retry(**retry_config)
 def get_lists(prefix_name):
-    """Получает список списков доменов из Cloudflare Gateway."""
     status, response = cloudflare_gateway_request("GET", "/lists")
-    return [l for l in response.get("result", []) if l["name"].startswith(prefix_name)]
-
+    lists = response["result"] or []
+    return [l for l in lists if l["name"].startswith(prefix_name)]
 
 @retry(**retry_config)
 def get_rules(rule_name_prefix):
-    """Получает список правил из Cloudflare Gateway."""
     status, response = cloudflare_gateway_request("GET", "/rules")
-    return [r for r in response.get("result", []) if r["name"].startswith(rule_name_prefix)]
-
+    rules = response["result"] or []
+    return [r for r in rules if r["name"].startswith(rule_name_prefix)]
 
 @retry(**retry_config)
 @rate_limited_request
 def delete_list(list_id):
-    """Удаляет список доменов из Cloudflare Gateway."""
-    start_time = time.time()
     endpoint = f"/lists/{list_id}"
     status, response = cloudflare_gateway_request("DELETE", endpoint)
-    end_time = time.time()
-    info(f"delete_list operation took {end_time - start_time:.2f} seconds")
     return response["result"]
-
 
 @retry(**retry_config)
 def delete_rule(rule_id):
-    """Удаляет правило из Cloudflare Gateway."""
-    start_time = time.time()
     endpoint = f"/rules/{rule_id}"
     status, response = cloudflare_gateway_request("DELETE", endpoint)
-    end_time = time.time()
-    info(f"delete_rule operation took {end_time - start_time:.2f} seconds")
     return response["result"]
-
 
 @retry(**retry_config)
 def get_list_items(list_id):
-    """Получает элементы списка доменов из Cloudflare Gateway."""
     endpoint = f"/lists/{list_id}/items?limit=1000"
     status, response = cloudflare_gateway_request("GET", endpoint)
-    return [i["value"] for i in response.get("result", [])]
+    items = response["result"] or []
+    return [i["value"] for i in items]
