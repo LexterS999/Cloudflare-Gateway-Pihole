083dfc059a8a850ce1cde4d49e83700824ee0cbc
LexterS999 <kaliostro18@gmail.com>
2024-08-17 22:41:26 +0500

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,14 +2,17 @@ import re
 from src import ids_pattern
 
 def split_domain_list(domains, chunk_size):
+    """Разбивает список доменов на части заданного размера."""
     for i in range(0, len(domains), chunk_size):
         yield domains[i:i + chunk_size]
 
 def safe_sort_key(list_item):
+    """Функция для безопасной сортировки элементов списка по имени, извлекая число из имени."""
     match = re.search(r'\d+', list_item["name"])
     return int(match.group()) if match else float('inf')
 
 def extract_list_ids(rule):
-        if not rule or not rule.get('traffic'):
-            return set()
-        return set(ids_pattern.findall(rule['traffic']))
+    """Извлекает ID списков из правила Cloudflare."""
+    if not rule or not rule.get('traffic'):
+        return set()
+    return set(ids_pattern.findall(rule['traffic']))
